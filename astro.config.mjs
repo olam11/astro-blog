11c373508daf66e4dd0a4ebae137ef0c279160c0
olam11 <malo.gryspeerdt+github@ikmail.com>
2026-02-21 18:30:40 +0100

    finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
+  site: "https://example.com",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [preact()],
 });
